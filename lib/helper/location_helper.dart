e38d06674eb3964ca1dd90d9a37b04cff70b285e
somtd <matsuda.so@gmail.com>
2019-09-29 23:21:23 +0900

    Adding google_maps_flutter

--- lib/helper/location_helper.dart
+++ lib/helper/location_helper.dart
@@ -5,6 +5,6 @@ class LocationHelper {
   static String generateLocationPreviewImage(
       {double latitude, double longitude}) {
     print(apiKey);
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=8&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$apiKey';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=8&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C$latitude,$longitude&markers=color:green%7Clabel:G%$latitude,$longitude&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$apiKey';
   }
 }
