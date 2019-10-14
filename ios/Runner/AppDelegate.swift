475b98a3678f95980ab6a5680d30d5d2239d6feb
somtd <matsuda.so@gmail.com>
2019-10-14 23:21:56 +0900

    Hide API key both android and iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Edit "GoogleMapsKey" in APIKey.swift
+    GMSServices.provideAPIKey(APIKey.GoogleMapsKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
